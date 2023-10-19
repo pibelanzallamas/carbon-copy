31d8b17fe52757a1e216df72030a6459b8cc2b4b
Brandon Miguel Castillo Llajamango <pibelanzallamas@MacBook-Pro-de-Brandon.local>
2023-10-19 19:58:50 -0300

    home mobile ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import "./styles/App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hola Soy brandon!!!!</h1>
+      <Home />
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="all">
+        <div className="portada">
+          <div className="grid">
+            <img className="logo" src="carboncopy.png"></img>
+            <img className="pinA" src="Group19.png"></img>
+            <img className="pinB" src="Group20.png"></img>
+          </div>
+        </div>
+        <div className="texto">
+          <p>Proyecto educativo inspirado en</p>
+        </div>
+        <div className="marca">
+          <img src="logoCarbon1.png"></img>
+        </div>
+        <div className="flecha">
+          <div className="iconoFecha"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
